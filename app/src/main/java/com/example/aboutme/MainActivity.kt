88c915decdd49c395f7901b2d040a348abb18a97
Olatunji <larexx40@gmail.com>
2020-04-05 05:33:47 +0100

    final commit

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        done_btn.setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View){
+        val editText= nickname_edit
+        val nicknameTextView= nickname_text
+
+        nicknameTextView.text=editText.text
+        editText.visibility= View.GONE
+        view.visibility=View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // To hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
+
     }
 }
